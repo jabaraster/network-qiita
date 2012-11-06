5aa39c544a7c199704669dac4ba92503e21a6ea7
Jabara <jabaraster@gmail.com>
2012-11-06 10:52:17 +0900

    mainte.

--- Jabara/QiitaTest.hs
+++ Jabara/QiitaTest.hs
@@ -9,14 +9,14 @@ import Data.Aeson
 import Network.HTTP.Conduit
 
 run = do
-  ma <- authenticate "jabaraster" "w9tau9Em"
+  ma <- authenticate "jabaraster" "xxxxxxxx"
   let auth = case ma of
                Left  m    -> Prelude.error $ show m
                Right auth -> auth
   getLoginUserInformation auth >>= print
 
 run2 = do
-  ma <- authenticate "jabaraster" "w9tau9Em"
+  ma <- authenticate "jabaraster" "xxxxxxxx"
   let auth = fromRight ma
   simpleHttp ("https://qiita.com/api/v1/user?token=" ++ (token auth))
 
