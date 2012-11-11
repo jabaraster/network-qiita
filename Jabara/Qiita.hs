231056c19f3e9e988e93f0fe006c6632e47a49c3
jabaraster <jabaraster@gmail.com>
2012-11-11 21:32:51 +0900

    #1 'getLoginUserInformation' がAPI設計の例

--- Jabara/Qiita.hs
+++ Jabara/Qiita.hs
@@ -3,6 +3,7 @@
 
 module Jabara.Qiita (
   authenticate
+  , withAuthenticate
   , getAnonymousRateLimit
   , getRateLimit
   , getLoginUserInformation
@@ -10,19 +11,24 @@ module Jabara.Qiita (
   , Auth(..)
   , RateLimit(..)
   , User(..)
+  , QiitaContext(..)
+-- for test
   ) where
 
 import Control.Applicative ((<*>))
 import Control.Monad.IO.Class (liftIO)
+import Control.Monad.State
 import Data.Aeson (Object, FromJSON, parseJSON, (.:), decode)
 import Data.Functor ((<$>))
 import Data.ByteString
 import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString.Char8 as C8
 import Data.Maybe
 import GHC.Exception (throw)
 import GHC.Generics (Generic)
 import Network.HTTP.Conduit
 import Network.HTTP.Types
+import Network.HTTP.Types.Header
 
 {- ------------------------------------------
  - data difinitions.
@@ -50,6 +56,8 @@ data User = User { name :: String
                  , following_users :: Int
                  , items :: Int
                  } deriving (Show, Eq, Generic)
+data QiitaContext = QiitaContext { auth :: Auth, rateLimit :: RateLimit }
+                    deriving (Show, Eq, Generic)
 
 {- ------------------------------------------
  - instance difinitions.
@@ -72,16 +80,41 @@ userUrl = endpoint ++ "/user"
  - public functions.
 ------------------------------------------- -}
 
+withAuthenticate :: ByteString -> ByteString
+                   -> (QiitaError -> RateLimit -> IO a)
+                   -> (QiitaContext -> IO a)
+                   -> IO a
+withAuthenticate user pass errorHandler mainOperation = do
+  -- リクエストの組み立て
+  req <- parseUrl authUrl
+           >>= return . urlEncodedBody [("url_name", user), ("password", pass)]
+           >>= \request -> return (request { checkStatus = checkStatus' })
+  -- 通信実行
+  res <- withManager $ \manager -> httpLbs req manager
+  -- レスポンスの処理
+  let rateLimit = parseRateLimit res
+--  let headers = responseHeaders res
+--  let limit = lookupIntValue "X-RateLimit-Limit" headers
+--  let remaining = lookupIntValue "X-RateLimit-Remaining" headers
+--  let rateLimit = RateLimit { remaining = remaining, limit = limit }
+  let eAuth = decodeJsonBody $ responseBody res
+  case eAuth of
+    Left  err  -> errorHandler err rateLimit
+    Right auth -> mainOperation $ QiitaContext { auth = auth, rateLimit = rateLimit }
 
 {- ------------------------------------------
  - Qiitaに認証を投げトークンを得る
 ------------------------------------------- -}
-authenticate :: ByteString -> ByteString -> IO (Either QiitaError Auth)
-authenticate user pass = liftIO $ do
-  parseUrl authUrl
-  >>= return . urlEncodedBody [("url_name", user), ("password", pass)]
-  >>= \request -> return (request { checkStatus = checkStatus' })
-  >>= requestJson
+-- authenticate :: ByteString -> ByteString -> IO (Either QiitaError Auth)
+authenticate user pass = do
+  req <- parseUrl authUrl
+           >>= return . urlEncodedBody [("url_name", user), ("password", pass)]
+           >>= \request -> return (request { checkStatus = checkStatus' })
+  res <- withManager $ \manager -> httpLbs req manager
+  let headers = responseHeaders res
+  let limit = lookupIntValue "X-RateLimit-Limit" headers
+  let remaining = lookupIntValue "X-RateLimit-Remaining" headers
+  return $ RateLimit { remaining = remaining, limit = limit }
 
 {- ------------------------------------------
  - 未ログインユーザのAPI実行回数を得る.
@@ -101,9 +134,20 @@ getRateLimit auth = simpleHttp (rateLimitUrl ++ (tok auth))
 {- ------------------------------------------
  - ログイン済みユーザの情報を得る.
 ------------------------------------------- -}
-getLoginUserInformation :: Auth -> IO User
-getLoginUserInformation auth = simpleHttp (userUrl ++ (tok auth))
-  >>= return . fromJust . decode
+getLoginUserInformation :: StateT QiitaContext IO User
+getLoginUserInformation = do
+  ctx <- get
+  req <- parseUrl (userUrl ++ (tok $ auth $ ctx))
+  res <- doRequest req
+
+  let rateLimit = parseRateLimit res
+  put $ ctx { rateLimit = rateLimit }
+
+  let body = responseBody res
+  return $ fromJust $ decode body
+
+doRequest req = withManager (\manager -> httpLbs req manager)
+
 {- ------------------------------------------
  - private functions.
 ------------------------------------------- -}
@@ -129,3 +173,15 @@ decodeJsonBody body = case decode body of
   Nothing   -> case (decode body :: Maybe QiitaError) of
                  Nothing -> Left $ QiitaError { errorMessage = "Unknown Error." }
                  Just e  -> Left e
+
+parseRateLimit :: Response b -> RateLimit
+parseRateLimit res = RateLimit {
+                       limit = lookupIntValue "X-RateLimit-Limit" $ responseHeaders res
+                       , remaining = lookupIntValue "X-RateLimit-Remaining" $ responseHeaders res
+                     }
+
+lookupIntValue :: HeaderName -> [Header] -> Int
+lookupIntValue headerName headers = case lookup headerName headers of
+                                          Nothing  -> 999
+                                          Just val -> read $ C8.unpack val
+
--- Jabara/QiitaTest.hs
+++ Jabara/QiitaTest.hs
@@ -4,10 +4,13 @@
 module Jabara.QiitaTest where
 
 import Jabara.Qiita
-import Data.Aeson
 
+import Control.Monad.State
+import Data.Aeson
+import qualified Data.ByteString.Lazy.Char8 as LC
 import Network.HTTP.Conduit
 
+{-
 run = do
   ma <- authenticate "jabaraster" "xxxxxxxx"
   let auth = case ma of
@@ -19,9 +22,31 @@ run2 = do
   ma <- authenticate "jabaraster" "xxxxxxxx"
   let auth = fromRight ma
   simpleHttp ("https://qiita.com/api/v1/user?token=" ++ (token auth))
+-}
+
+run = withAuthenticate "jabaraster" "w9tau9Em"
+        (\err limit -> print err >> print limit)
+        (\ctx -> evalStateT core ctx)
+
+core = do
+  ctx1 <- get
+  liftIO $ putStrLn ("Pre: " ++ (show ctx1))
+
+  user <- getLoginUserInformation'
+  liftIO $ print user
+  ctx2 <- get
+  liftIO $ putStrLn ("Post: " ++ (show ctx2))
+
+  user' <- getLoginUserInformation'
+  liftIO $ print user'
+  ctx3 <- get
+  liftIO $ putStrLn ("Post: " ++ (show ctx3))
 
 fromRight :: (Show l) => Either l r -> r
 fromRight (Left left) = Prelude.error $ show left
 fromRight (Right right) = right
 
 instance ToJSON User
+
+j = LC.pack "{\"name\":\"jabaraster\",\"url_name\":\"jabaraster\",\"profile_image_url\":\"https://si0.twimg.com/profile_images/1272321155/duke_globe_normal.gif\",\"url\":\"http://qiita.com/users/jabaraster\",\"description\":\"\",\"website_url\":\"\",\"organization\":\"\",\"location\":\"Kumamoto, Japan\",\"facebook\":\"\",\"linkedin\":\"\",\"twitter\":\"jabaraster\",\"github\":\"jabaraster\",\"followers\":2,\"following_users\":4,\"items\":8}"
+
